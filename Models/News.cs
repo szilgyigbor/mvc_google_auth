7b7ab988c25b18761b5b7ee5600a8ffe9b21bccb
Gábor <szilgyigbor@gmail.com>
2023-09-26 10:29:21 +0200

    Add News model with properties.

---
+++ Models/News.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCGoogleAuth.Models
+{
+    public class News
+    {
+        [Required]
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateCreated { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateModified { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+    }
+}
