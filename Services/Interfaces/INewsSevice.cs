0fa41bdb85a2fb326e4f8ed0d83c2f3acd3455d7
Gábor <szilgyigbor@gmail.com>
2023-09-26 18:02:55 +0200

    Add INewsService interface.

---
+++ Services/Interfaces/INewsSevice.cs
@@ -0,0 +1,15 @@
+﻿using MVCGoogleAuth.Models;
+
+namespace MVCGoogleAuth.Services.Interfaces
+{
+    public interface INewsSevice
+    {
+        News GetNewsById(int id);
+
+        News CreateNewsAsync(News news);
+
+        News UpdateNewsAsync(News news);
+
+        bool DeleteNewsAsync(int id);
+    }
+}
