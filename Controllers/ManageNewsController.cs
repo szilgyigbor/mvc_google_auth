fc8252d8981db3ffbb21ce52b3f8892be988dabc
Gábor <szilgyigbor@gmail.com>
2023-09-26 13:26:02 +0200

    Add ManageNewsController.

---
+++ Controllers/ManageNewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCGoogleAuth.Controllers
+{
+    public class ManageNewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
