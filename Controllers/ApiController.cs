4fe292d08c4d4c77fe5d749d248f1cb3227e7d53
Gábor <szilgyigbor@gmail.com>
2023-09-27 08:58:47 +0200

    Add ApiController with the required methods.

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCGoogleAuth.Models;
+using MVCGoogleAuth.Services.Interfaces;
+
+namespace MVCGoogleAuth.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        private readonly INewsSevice _newsService;
+
+        public ApiController(INewsSevice newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [Route("api/getnews")]
+        [HttpGet]
+        public IActionResult GetNews()
+        {
+            return Ok(_newsService.GetNews());
+        }
+
+        [Route("api/getnews/{id}")]
+        [HttpGet]
+        public IActionResult GetNewsById(int id)
+        {
+            return Ok(_newsService.GetNewsById(id));
+        }
+
+        [Route("api/createnews")]
+        [HttpPost]
+        public IActionResult CreateNews([FromBody] News news)
+        {
+            return Ok(_newsService.CreateNewsAsync(news));
+        }
+
+        [Route("api/updatenews")]
+        [HttpPut]
+        public IActionResult UpdateNews([FromBody] News news)
+        {
+            return Ok(_newsService.UpdateNewsAsync(news));
+        }
+
+        [Route("api/deletenews/{id}")]
+        [HttpDelete]
+        public IActionResult DeleteNews(int id)
+        {
+            return Ok(_newsService.DeleteNewsAsync(id));
+        }
+    }
+}
