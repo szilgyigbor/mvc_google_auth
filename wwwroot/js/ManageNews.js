8593e3de6a853e198e3061d4625a574759096780
Gábor <szilgyigbor@gmail.com>
2023-09-27 12:26:26 +0200

    Add updateNews function to ManageNews.js.

--- wwwroot/js/ManageNews.js
+++ wwwroot/js/ManageNews.js
@@ -1,3 +1,7 @@
+let isUpdating = false;
+let updatingId = null;
+const submitButton = document.getElementById('subbutton');
+
 document.getElementById('newsForm').addEventListener('submit', function (event) {
     event.preventDefault();
 
@@ -13,22 +17,45 @@ document.getElementById('newsForm').addEventListener('submit', function (event) 
 
     event.preventDefault();
 
-    fetch("https://localhost:44335/api/createnews", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ title: title.value, content: content.value, imageUrl: imageUrl.value })
-    })
-        .then(response => response.json())
-        .then(data => {
-            fetchNews();
-            title.value = '';
-            content.value = '';
-            imageUrl.value = '';
-        });
-    
+    if (isUpdating) {
+        fetch(`https://localhost:44335/api/updatenews/${updatingId}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ title: title.value, content: content.value, imageUrl: imageUrl.value })
+        })
+            .then(response => response.json())
+            .then(data => {
+                fetchNews();
+                title.value = '';
+                content.value = '';
+                imageUrl.value = '';
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+
+        isUpdating = false;
+        updatingId = null;
+        submitButton.innerHTML = "Add news";
+
+    } else {
+        fetch("https://localhost:44335/api/createnews", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ title: title.value, content: content.value, imageUrl: imageUrl.value })
+        })
+            .then(response => response.json())
+            .then(data => {
+                fetchNews();
+                title.value = '';
+                content.value = '';
+                imageUrl.value = '';
+            });
+    }
 });
+
 
 function fetchNews() {
     fetch("https://localhost:44335/api/getnews")
@@ -39,9 +66,10 @@ function fetchNews() {
 
             data.forEach(news => {
                 const newsItem = document.createElement('div');
+                newsItem.dataset.id = news.id;
                 newsItem.innerHTML = `<p>
-                Id: ${news.id}
-                <h5>${news.title} <button class="delete-button" data-id="${news.id}">Delete</button></h5>
+                Id: ${news.id} <button onclick="updateNews(${news.id})">Update News</button>
+                <h5>${news.title} <button class="delete-button" data-id="${news.id}">Delete News</button></h5>
                 Content: ${news.content}
                 <br>
                 Image: ${news.imageUrl}
@@ -50,7 +78,7 @@ function fetchNews() {
                 <br>
                 Modified: ${news.dateModified}
                 <br>
-                Author: ${news.author}</p>`;
+                Author: ${news.author}</p>`; 
                 newsList.appendChild(newsItem);
             });
         });
@@ -64,6 +92,32 @@ function deleteNews(id) {
         .then(data => {
             console.log('Deleted:', data);
             fetchNews()
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function updateNews(id) {
+    isUpdating = true;
+    updatingId = id;
+    console.log(id);
+    
+    fetch(`https://localhost:44335/api/getnews/${id}`, {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('News to update:', data);
+            const title = document.getElementById('title');
+            const content = document.getElementById('content');
+            const imageUrl = document.getElementById('imageurl');
+            const button = document.getElementById('subbutton');
+
+            title.value = data.title;
+            content.value = data.content;
+            imageUrl.value = data.imageUrl;
+            button.innerHTML = "Update this news";
         })
         .catch((error) => {
             console.error('Error:', error);
